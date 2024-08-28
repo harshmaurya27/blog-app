4c978737baf72f059f55aa52391ac2e44e5e2009
unknown <mauryaharsh464@gmail.com>
2024-08-28 11:20:34 +0530

    Page

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
-const page = () => {
+const Page = () => {
   const [blogs, setBlogs] = useState([]);
 
   const fetchBlog = async () => {
@@ -70,4 +70,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
